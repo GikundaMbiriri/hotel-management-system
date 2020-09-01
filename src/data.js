bb7dbd6a1b79d95f8aa8c3953ce53ec371bec28b
Your Name <you@example.com>
2020-09-01 18:57:09 +0300

    first commit

--- src/data.js
+++ src/data.js
@@ -18,14 +18,14 @@ import img12 from "./images/room-12.jpeg";
 export default [
   {
     sys: {
-      id: "1"
+      id: "1",
     },
     fields: {
       name: "single economy",
       slug: "single-economy",
       type: "single",
       price: 100,
-      size: 200,
+      size: 200000,
       capacity: 1,
       pets: false,
       breakfast: false,
@@ -39,49 +39,49 @@ export default [
         "Complimentary refreshments",
         "Adequate safety/security",
         "Internet",
-        "Comfortable beds"
+        "Comfortable beds",
       ],
       images: [
         {
           fields: {
             file: {
-              url: img1
-            }
-          }
+              url: img1,
+            },
+          },
         },
         {
           fields: {
             file: {
-              url: room2
-            }
-          }
+              url: room2,
+            },
+          },
         },
         {
           fields: {
             file: {
-              url: room3
-            }
-          }
+              url: room3,
+            },
+          },
         },
         {
           fields: {
             file: {
-              url: room4
-            }
-          }
-        }
-      ]
-    }
+              url: room4,
+            },
+          },
+        },
+      ],
+    },
   },
   {
     sys: {
-      id: "2"
+      id: "2",
     },
     fields: {
       name: "single basic",
       slug: "single-basic",
       type: "single",
-      price: 150,
+      price: 150000,
       size: 250,
       capacity: 1,
       pets: false,
@@ -96,49 +96,49 @@ export default [
         "Complimentary refreshments",
         "Adequate safety/security",
         "Internet",
-        "Comfortable beds"
+        "Comfortable beds",
       ],
       images: [
         {
           fields: {
             file: {
-              url: img2
-            }
-          }
+              url: img2,
+            },
+          },
         },
         {
           fields: {
             file: {
-              url: room2
-            }
-          }
+              url: room2,
+            },
+          },
         },
         {
           fields: {
             file: {
-              url: room3
-            }
-          }
+              url: room3,
+            },
+          },
         },
         {
           fields: {
             file: {
-              url: room4
-            }
-          }
-        }
-      ]
-    }
+              url: room4,
+            },
+          },
+        },
+      ],
+    },
   },
   {
     sys: {
-      id: "3"
+      id: "3",
     },
     fields: {
       name: "single standard",
       slug: "single-standard",
       type: "single",
-      price: 250,
+      price: 250000,
       size: 300,
       capacity: 1,
       pets: true,
@@ -153,49 +153,49 @@ export default [
         "Complimentary refreshments",
         "Adequate safety/security",
         "Internet",
-        "Comfortable beds"
+        "Comfortable beds",
       ],
       images: [
         {
           fields: {
             file: {
-              url: img3
-            }
-          }
+              url: img3,
+            },
+          },
         },
         {
           fields: {
             file: {
-              url: room2
-            }
-          }
+              url: room2,
+            },
+          },
         },
         {
           fields: {
             file: {
-              url: room3
-            }
-          }
+              url: room3,
+            },
+          },
         },
         {
           fields: {
             file: {
-              url: room4
-            }
-          }
-        }
-      ]
-    }
+              url: room4,
+            },
+          },
+        },
+      ],
+    },
   },
   {
     sys: {
-      id: "4"
+      id: "4",
     },
     fields: {
       name: "single deluxe",
       slug: "single-deluxe",
       type: "single",
-      price: 300,
+      price: 300000,
       size: 400,
       capacity: 1,
       pets: true,
@@ -210,49 +210,49 @@ export default [
         "Complimentary refreshments",
         "Adequate safety/security",
         "Internet",
-        "Comfortable beds"
+        "Comfortable beds",
       ],
       images: [
         {
           fields: {
             file: {
-              url: img4
-            }
-          }
+              url: img4,
+            },
+          },
         },
         {
           fields: {
             file: {
-              url: room2
-            }
-          }
+              url: room2,
+            },
+          },
         },
         {
           fields: {
             file: {
-              url: room3
-            }
-          }
+              url: room3,
+            },
+          },
         },
         {
           fields: {
             file: {
-              url: room4
-            }
-          }
-        }
-      ]
-    }
+              url: room4,
+            },
+          },
+        },
+      ],
+    },
   },
   {
     sys: {
-      id: "5"
+      id: "5",
     },
     fields: {
       name: "double economy",
       slug: "double-economy",
       type: "double",
-      price: 200,
+      price: 200000,
       size: 300,
       capacity: 2,
       pets: false,
@@ -267,49 +267,49 @@ export default [
         "Complimentary refreshments",
         "Adequate safety/security",
         "Internet",
-        "Comfortable beds"
+        "Comfortable beds",
       ],
       images: [
         {
           fields: {
             file: {
-              url: img5
-            }
-          }
+              url: img5,
+            },
+          },
         },
         {
           fields: {
             file: {
-              url: room2
-            }
-          }
+              url: room2,
+            },
+          },
         },
         {
           fields: {
             file: {
-              url: room3
-            }
-          }
+              url: room3,
+            },
+          },
         },
         {
           fields: {
             file: {
-              url: room4
-            }
-          }
-        }
-      ]
-    }
+              url: room4,
+            },
+          },
+        },
+      ],
+    },
   },
   {
     sys: {
-      id: "6"
+      id: "6",
     },
     fields: {
       name: "double basic",
       slug: "double-basic",
       type: "double",
-      price: 250,
+      price: 250000,
       size: 350,
       capacity: 2,
       pets: false,
@@ -324,49 +324,49 @@ export default [
         "Complimentary refreshments",
         "Adequate safety/security",
         "Internet",
-        "Comfortable beds"
+        "Comfortable beds",
       ],
       images: [
         {
           fields: {
             file: {
-              url: img6
-            }
-          }
+              url: img6,
+            },
+          },
         },
         {
           fields: {
             file: {
-              url: room2
-            }
-          }
+              url: room2,
+            },
+          },
         },
         {
           fields: {
             file: {
-              url: room3
-            }
-          }
+              url: room3,
+            },
+          },
         },
         {
           fields: {
             file: {
-              url: room4
-            }
-          }
-        }
-      ]
-    }
+              url: room4,
+            },
+          },
+        },
+      ],
+    },
   },
   {
     sys: {
-      id: "7"
+      id: "7",
     },
     fields: {
       name: "double standard",
       slug: "double-standard",
       type: "double",
-      price: 300,
+      price: 300000,
       size: 400,
       capacity: 2,
       pets: true,
@@ -381,49 +381,49 @@ export default [
         "Complimentary refreshments",
         "Adequate safety/security",
         "Internet",
-        "Comfortable beds"
+        "Comfortable beds",
       ],
       images: [
         {
           fields: {
             file: {
-              url: img7
-            }
-          }
+              url: img7,
+            },
+          },
         },
         {
           fields: {
             file: {
-              url: room2
-            }
-          }
+              url: room2,
+            },
+          },
         },
         {
           fields: {
             file: {
-              url: room3
-            }
-          }
+              url: room3,
+            },
+          },
         },
         {
           fields: {
             file: {
-              url: room4
-            }
-          }
-        }
-      ]
-    }
+              url: room4,
+            },
+          },
+        },
+      ],
+    },
   },
   {
     sys: {
-      id: "8"
+      id: "8",
     },
     fields: {
       name: "double deluxe",
       slug: "double-deluxe",
       type: "double",
-      price: 400,
+      price: 400000,
       size: 500,
       capacity: 2,
       pets: true,
@@ -438,49 +438,49 @@ export default [
         "Complimentary refreshments",
         "Adequate safety/security",
         "Internet",
-        "Comfortable beds"
+        "Comfortable beds",
       ],
       images: [
         {
           fields: {
             file: {
-              url: img8
-            }
-          }
+              url: img8,
+            },
+          },
         },
         {
           fields: {
             file: {
-              url: room2
-            }
-          }
+              url: room2,
+            },
+          },
         },
         {
           fields: {
             file: {
-              url: room3
-            }
-          }
+              url: room3,
+            },
+          },
         },
         {
           fields: {
             file: {
-              url: room4
-            }
-          }
-        }
-      ]
-    }
+              url: room4,
+            },
+          },
+        },
+      ],
+    },
   },
   {
     sys: {
-      id: "9"
+      id: "9",
     },
     fields: {
       name: "family economy",
       slug: "family-economy",
       type: "family",
-      price: 300,
+      price: 300000,
       size: 500,
       capacity: 3,
       pets: false,
@@ -495,49 +495,49 @@ export default [
         "Complimentary refreshments",
         "Adequate safety/security",
         "Internet",
-        "Comfortable beds"
+        "Comfortable beds",
       ],
       images: [
         {
           fields: {
             file: {
-              url: img9
-            }
-          }
+              url: img9,
+            },
+          },
         },
         {
           fields: {
             file: {
-              url: room2
-            }
-          }
+              url: room2,
+            },
+          },
         },
         {
           fields: {
             file: {
-              url: room3
-            }
-          }
+              url: room3,
+            },
+          },
         },
         {
           fields: {
             file: {
-              url: room4
-            }
-          }
-        }
-      ]
-    }
+              url: room4,
+            },
+          },
+        },
+      ],
+    },
   },
   {
     sys: {
-      id: "10"
+      id: "10",
     },
     fields: {
       name: "family basic",
       slug: "family-basic",
       type: "family",
-      price: 350,
+      price: 350000,
       size: 550,
       capacity: 4,
       pets: false,
@@ -552,49 +552,49 @@ export default [
         "Complimentary refreshments",
         "Adequate safety/security",
         "Internet",
-        "Comfortable beds"
+        "Comfortable beds",
       ],
       images: [
         {
           fields: {
             file: {
-              url: img10
-            }
-          }
+              url: img10,
+            },
+          },
         },
         {
           fields: {
             file: {
-              url: room2
-            }
-          }
+              url: room2,
+            },
+          },
         },
         {
           fields: {
             file: {
-              url: room3
-            }
-          }
+              url: room3,
+            },
+          },
         },
         {
           fields: {
             file: {
-              url: room4
-            }
-          }
-        }
-      ]
-    }
+              url: room4,
+            },
+          },
+        },
+      ],
+    },
   },
   {
     sys: {
-      id: "11"
+      id: "11",
     },
     fields: {
       name: "family standard",
       slug: "family-standard",
       type: "family",
-      price: 400,
+      price: 400000,
       size: 600,
       capacity: 5,
       pets: true,
@@ -609,49 +609,49 @@ export default [
         "Complimentary refreshments",
         "Adequate safety/security",
         "Internet",
-        "Comfortable beds"
+        "Comfortable beds",
       ],
       images: [
         {
           fields: {
             file: {
-              url: img11
-            }
-          }
+              url: img11,
+            },
+          },
         },
         {
           fields: {
             file: {
-              url: room2
-            }
-          }
+              url: room2,
+            },
+          },
         },
         {
           fields: {
             file: {
-              url: room3
-            }
-          }
+              url: room3,
+            },
+          },
         },
         {
           fields: {
             file: {
-              url: room4
-            }
-          }
-        }
-      ]
-    }
+              url: room4,
+            },
+          },
+        },
+      ],
+    },
   },
   {
     sys: {
-      id: "12"
+      id: "12",
     },
     fields: {
       name: "family deluxe",
       slug: "family-deluxe",
       type: "family",
-      price: 500,
+      price: 500000,
       size: 700,
       capacity: 6,
       pets: true,
@@ -666,49 +666,49 @@ export default [
         "Complimentary refreshments",
         "Adequate safety/security",
         "Internet",
-        "Comfortable beds"
+        "Comfortable beds",
       ],
       images: [
         {
           fields: {
             file: {
-              url: img12
-            }
-          }
+              url: img12,
+            },
+          },
         },
         {
           fields: {
             file: {
-              url: room2
-            }
-          }
+              url: room2,
+            },
+          },
         },
         {
           fields: {
             file: {
-              url: room3
-            }
-          }
+              url: room3,
+            },
+          },
         },
         {
           fields: {
             file: {
-              url: room4
-            }
-          }
-        }
-      ]
-    }
+              url: room4,
+            },
+          },
+        },
+      ],
+    },
   },
   {
     sys: {
-      id: "13"
+      id: "13",
     },
     fields: {
       name: "presidential",
       slug: "presidential-room",
       type: "presidential",
-      price: 600,
+      price: 6000000,
       size: 1000,
       capacity: 10,
       pets: true,
@@ -723,38 +723,38 @@ export default [
         "Complimentary refreshments",
         "Adequate safety/security",
         "Internet",
-        "Comfortable beds"
+        "Comfortable beds",
       ],
       images: [
         {
           fields: {
             file: {
-              url: room1
-            }
-          }
+              url: room1,
+            },
+          },
         },
         {
           fields: {
             file: {
-              url: room2
-            }
-          }
+              url: room2,
+            },
+          },
         },
         {
           fields: {
             file: {
-              url: room3
-            }
-          }
+              url: room3,
+            },
+          },
         },
         {
           fields: {
             file: {
-              url: room4
-            }
-          }
-        }
-      ]
-    }
-  }
+              url: room4,
+            },
+          },
+        },
+      ],
+    },
+  },
 ];
